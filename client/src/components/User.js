dc0894a45a530b9ecf4ae13d22a2d9aa7996faec
Eleanor Kelman <ekelman3@gmail.com>
2023-10-28 11:01:37 -0400

    finished client-side CRUD fetching

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,1 +1,18 @@
 import React, { useState, useEffect } from "react";
+
+function User() {
+
+    const [userID, setUserID] = useState(1)
+
+    useEffect(() => {
+        fetch(`/users/${userID}`)
+            .then((resp) => resp.json())
+            .then((data) => console.log(data))
+    }, [userID])
+
+    return (
+        <div>
+            <input placeholder = "id" onChange = {(e) => selectID(e)} />
+        </div>
+    )
+}
