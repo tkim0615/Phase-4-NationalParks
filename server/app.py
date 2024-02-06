560baaeb16a2b008487e4414bb3d722c9d149226
tyler kim <yoonsoo.kim11@gmail.com>
2024-02-05 21:25:33 -0500

    completed

--- server/app.py
+++ server/app.py
@@ -1,8 +1,12 @@
 from flask import request, make_response
 
 from werkzeug.exceptions import NotFound
+from flask_restful import Api, Resource
+from models import db, User, NationalPark, UserVisitedPark
+
 
 from config import app
+api = Api(app)
 
 # unique error message upon nonexistent server-side route
 @app.errorhandler(NotFound)
@@ -16,7 +20,85 @@ def route_not_found(e):
 
 @app.route('/')
 def home():
-    return ""
+    return "home"
+
+
+class Users(Resource):
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()]
+        return make_response(users, 200)
+    def post(self):
+        data = request.get_json()
+        try:
+            new_user = User(
+                username = data['username'],
+                password= data['password']
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            return make_response(new_user.to_dict(rules=('-user_visited_parks',)), 201)
+
+        except ValueError:
+            return make_response({
+                'error': 'validation error'
+            })
+
+
+class NationalParks(Resource):
+    def get(self):
+        national_parks = [park.to_dict() for park in NationalPark.query.all()]
+        return make_response(national_parks, 200)
+    
+class UsersById(Resource):
+    def get(self, id):
+        user = User.query.filter(User.id == id).first()
+        if user:
+            return make_response(user.to_dict(), 200)
+
+        else:
+            return make_response({
+                'Error': 'No user found'
+            }, 404)
+
+
+    def patch(self, id):
+        user = User.query.filter(User.id == id).first()
+        if user:
+            try:
+                data = request.get_json()
+                for attr in data:
+                    setattr(user, attr, data[attr])
+                db.session.commit()
+                return make_response(user.to_dict(), 202)
+            except ValueError:
+                return make_response({
+                    'error': 'Validation error'
+                }, 400)
+        else:
+            return make_response({
+                'error': 'no user found'
+            }, 404)
+
+class NationalParksById(Resource):
+    def delete(self, id):
+        park = NationalPark.query.filter(NationalPark.id == id).first()
+        if park:
+            db.session.delete(park)
+            db.session.commit()
+            return make_response({}, 204)
+        return make_response({
+            'error': 'no national park found'
+        }, 404)
+
+
+
+
+api.add_resource(NationalParksById, '/national_parks/<int:id>')
+api.add_resource(UsersById, '/users/<int:id>')
+api.add_resource(NationalParks, '/national_parks')
+api.add_resource(Users, '/users')
+
+
 
 if __name__ == '__main__':
-    app.run(port = 5555, debug = True)
+    app.run(port = 5556, debug = True)
--- server/models.py
+++ server/models.py
@@ -10,3 +10,74 @@ metadata = MetaData(naming_convention = {
 
 # creates SQLAlchemy DB ORM
 db = SQLAlchemy(metadata = metadata)
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username= db.Column(db.String, nullable=False)
+    password= db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    #add relation
+    user_visited_parks = db.relationship('UserVisitedPark', back_populates='user', cascade = 'all, delete')
+
+    #add serializer
+    serialize_rules=('-user_visited_parks.user',)
+
+    #add validation
+    @validates('username')
+    def validate_username(self, key, value):
+        if not 4<=len(value)<=14:
+            raise ValueError('username must be of length 4 to 14 inclusive')
+        return value
+
+class NationalPark(db.Model, SerializerMixin):
+    __tablename__ = 'national_parks'
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String)
+    state= db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    #add relation
+    user_visited_parks = db.relationship('UserVisitedPark', back_populates='national_park', cascade = 'all, delete')
+
+
+    #add serializer
+    serialize_rules=('-user_visited_parks.national_park',)
+
+
+class UserVisitedPark(db.Model, SerializerMixin):
+    __tablename__ = 'user_visited_parks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date_of_visit = db.Column(db.String)
+
+    #add relationship
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    park_id = db.Column(db.Integer, db.ForeignKey('national_parks.id'))
+
+    user = db.relationship('User', back_populates='user_visited_parks')
+    national_park = db.relationship('NationalPark', back_populates='user_visited_parks')
+
+    #add serializer
+    serialize_rules=('-user.user_visited_parks', '-national_park.user_visited_parks')
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+    
